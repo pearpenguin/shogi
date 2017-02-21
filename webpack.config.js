0601fa69f75c31881ed7a47d63ac5a5fe0cba3f1
Kenley Cheung <cheung.kenley@gmail.com>
2017-02-22 11:49:43 +1300

    Added babel to transpile ES6, using env preset. Integrated with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,14 @@ var path = require('path');
 var webpack = require("webpack");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+  	entry: './src/index.js',
+  	output: {
+    	filename: 'app.js',
+    	path: path.resolve(__dirname, 'dist')
+  	},
+  	module: {
+  		loaders: [
+  			{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+  		]
+  	}
 };
