54c4838cff2f8218fd36b560c00680a3671f4cbf
Kenley Cheung <cheung.kenley@gmail.com>
2017-02-21 16:44:16 +1300

    Add basic webpack config and sublime text project

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+var webpack = require("webpack");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
